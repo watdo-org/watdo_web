41d1c696b2ce19d3bff9cded750885941eb7430f
Nie Tsuki <nie.0x6708@gmail.com>
2025-08-23 01:46:44 +0800

    timeline

--- src/lib/server.ts
+++ src/lib/server.ts
@@ -3,12 +3,14 @@ import FirebaseAuth from "$lib/firebase/auth";
 export default class Server {
   static baseUrl = "https://watdo.up.railway.app/v1";
 
-  static async fetch(path: string, method: string = "GET") {
+  static async fetch(path: string, method: string = "GET", body: any = null) {
     return await fetch(`${Server.baseUrl}${path}`, {
       method: method,
       headers: {
         Authorization: `Bearer ${await FirebaseAuth.getUserIdToken()}`,
+        "Content-Type": "application/json",
       },
+      body: body ? JSON.stringify(body) : null,
     });
   }
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,53 @@
 <script lang="ts">
+    import { onMount } from "svelte";
+    import Server from "$lib/server";
     import TopBar from "$lib/components/TopBar.svelte";
+
+    let blocks = $state([]);
+    let activeBlocks = $state([]);
+
+    onMount(async () => {
+        const res = await Server.fetch("/blocks/");
+        blocks = await res.json();
+
+        for (const block of blocks) {
+            const res = await Server.fetch(
+                `/blocks/${block["id"]}/call/evaluate_schedule`,
+                "POST",
+                {
+                    "relative_base": new Date().toISOString(),
+                    "evaluation_date": new Date().toISOString(),
+                }
+            );
+
+            if (await res.json()) {
+                activeBlocks.push(block["id"]);
+            }
+        }
+    });
 </script>
 
 <TopBar />
+
+<div class="flex mt-14">
+    <div>
+        {#each blocks as block (block["id"])}
+            {#if activeBlocks.includes(block["id"])}
+                <div class="w-full relative">
+                    <pre>{JSON.stringify(block["parsed"], null, "\t")}</pre>
+
+                    <button
+                        class="absolute top-0 right-0 cursor-pointer"
+                        onclick={async () => {
+                            await Server.fetch(
+                                `/blocks/${block["id"]}/call/apply_action`,
+                                "POST",
+                                { "action": "remove" }
+                            );
+                        }}
+                    >Remove</button>
+                </div>
+            {/if}
+        {/each}
+    </div>
+</div>
