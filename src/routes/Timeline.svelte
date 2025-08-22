edf13620c4dbc207e4465037fcf52318684b886a
Nie Tsuki <nie.0x6708@gmail.com>
2025-08-22 23:03:39 +0800

    Add editor page

--- src/routes/Timeline.svelte
+++ src/routes/Timeline.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+    import { goto } from "$app/navigation";
+    import { resolve } from "$app/paths";
     import FirebaseAuth from "$lib/firebase/auth";
 
     async function handleSignOut() {
@@ -9,13 +11,19 @@
 <div class="font-[Kalam]">
     <div class="absolute top-0 left-0 w-full h-14 bg-white shadow-xs">
         <div class="flex items-center h-full justify-center">
-            <button class="m-4 cursor-pointer text-neutral-600">
+            <button
+                class="m-4 cursor-pointer text-neutral-600"
+                onclick={() => goto(resolve("/"))}
+            >
                 Timeline
             </button>
             <button class="m-4 cursor-pointer text-neutral-600">
                 Blocks
             </button>
-            <button class="m-4 cursor-pointer text-neutral-600">
+            <button
+                class="m-4 cursor-pointer text-neutral-600"
+                onclick={() => goto(resolve("/editor"))}
+            >
                 Editor
             </button>
             <button class="m-4 cursor-pointer text-neutral-600">
@@ -24,7 +32,7 @@
         </div>
 
         <div class="flex items-center absolute top-0 right-0 h-full px-6 py-3">
-            <button on:click={handleSignOut} class="text-neutral-900 cursor-pointer">
+            <button onclick={handleSignOut} class="text-neutral-900 cursor-pointer">
                 Sign Out
             </button>
         </div>
---
+++ src/routes/editor/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import CodeEditor from "$lib/components/code-editor/code-editor.svelte";
+
+    let editor: CodeEditor | undefined | null;
+    let code = $state("");
+
+    $effect(() => {
+        if (editor) {
+            editor.setup();
+        }
+    });
+</script>
+
+<div class="w-full h-[600px]">
+    <CodeEditor
+        bind:this={editor}
+        bind:code
+        language="plaintext"
+        theme="vs"
+        onSave={() => {}}
+    />
+</div>
