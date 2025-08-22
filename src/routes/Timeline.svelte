ddfc0a436abf17c5682d93ba70005939a743b720
Nie Tsuki <nie.0x6708@gmail.com>
2025-08-22 23:15:47 +0800

    TopBar component

--- src/routes/Timeline.svelte
+++ src/routes/Timeline.svelte
@@ -1,40 +1,5 @@
 <script lang="ts">
-    import { goto } from "$app/navigation";
-    import { resolve } from "$app/paths";
-    import FirebaseAuth from "$lib/firebase/auth";
-
-    async function handleSignOut() {
-        await FirebaseAuth.signOut();
-    }
+    import TopBar from "$lib/components/TopBar.svelte";
 </script>
 
-<div class="font-[Kalam]">
-    <div class="absolute top-0 left-0 w-full h-14 bg-white shadow-xs">
-        <div class="flex items-center h-full justify-center">
-            <button
-                class="m-4 cursor-pointer text-neutral-600"
-                onclick={() => goto(resolve("/"))}
-            >
-                Timeline
-            </button>
-            <button class="m-4 cursor-pointer text-neutral-600">
-                Blocks
-            </button>
-            <button
-                class="m-4 cursor-pointer text-neutral-600"
-                onclick={() => goto(resolve("/editor"))}
-            >
-                Editor
-            </button>
-            <button class="m-4 cursor-pointer text-neutral-600">
-                Help
-            </button>
-        </div>
-
-        <div class="flex items-center absolute top-0 right-0 h-full px-6 py-3">
-            <button onclick={handleSignOut} class="text-neutral-900 cursor-pointer">
-                Sign Out
-            </button>
-        </div>
-    </div>
-</div>
+<TopBar />
--- src/routes/editor/+page.svelte
+++ src/routes/editor/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import CodeEditor from "$lib/components/code-editor/code-editor.svelte";
+    import TopBar from "$lib/components/TopBar.svelte";
 
     let editor: CodeEditor | undefined | null;
     let code = $state("");
@@ -11,7 +12,9 @@
     });
 </script>
 
-<div class="w-full h-[600px]">
+<TopBar />
+
+<div class="w-full h-[600px] mt-14">
     <CodeEditor
         bind:this={editor}
         bind:code
